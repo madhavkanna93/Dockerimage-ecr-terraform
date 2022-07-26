2eea8a64867bba19eebfa51b7db902892847ff35
madhavkanna93 <109338181+madhavkanna93@users.noreply.github.com>
2022-07-27 02:58:31 +0530

    def.tf updated

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,12 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "awsgitzipfile1"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
