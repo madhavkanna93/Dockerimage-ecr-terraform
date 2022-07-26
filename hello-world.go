169eb2c806bf64fa52a7682f1b49a66f14371dad
madhavkanna93 <109338181+madhavkanna93@users.noreply.github.com>
2022-07-27 02:29:25 +0530

    docker update

---
+++ hello-world.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "net/http"
+
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Service Health Check : Pass</h1>")
+}
+
+func main() {
+	http.HandleFunc("/",index)
+	http.HandleFunc("/health",health)
+	fmt.Println("Server Starting...")
+	http.ListenAndServe(":8080",nil)
+}
